c3a095d023225acff94c1e3b3211d4bf4485ba32
ElianNasatto <elian.nasa123@gmail.com>
2019-08-07 11:17:16 -0300

    Iniciado desenvolvimento
    Criação da solução, modelo de pessoas

---
+++ ExemploAjax02/Model/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    [Table("pessoas")]
+    public class Pessoa
+    {
+        [Key, Column("id")]
+        public int Id { get; set; }
+
+        [Column("nome")]
+        public string Nome { get; set; }
+
+        [Column("cpf")]
+        public string CPF { get; set; }
+        
+    }
+}
